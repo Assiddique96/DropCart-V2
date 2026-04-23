8d899ff4ce683bbe052c50f1633479747aa43688
Vincent <122548823+Vinnnnce@users.noreply.github.com>
2026-04-23 23:35:29 +0300

    Update AdminNavbar.jsx

--- components/admin/AdminNavbar.jsx
+++ components/admin/AdminNavbar.jsx
@@ -10,7 +10,7 @@ const AdminNavbar = () => {
     return (
         <div className="flex items-center justify-between px-6 sm:px-12 py-3 border-b border-slate-200 dark:border-slate-700 bg-white dark:bg-slate-950 transition-all">
             <Link href="/" className="relative text-4xl font-semibold text-slate-700">
-                <span className="text-gray-600 dark:text-slate-200">Drop</span>Cart<span className="text-gray-300 dark:text-slate-400 text-5xl leading-0">.</span>
+                <span className="text-gray-600 dark:text-slate-200">Shp</span>inx<span className="text-gray-300 dark:text-slate-400 text-5xl leading-0">.</span>
                 <p className="absolute text-xs font-semibold -top-1 -right-13 px-3 p-0.5 rounded-full flex items-center gap-2 text-white bg-gray-500">
                     Admin
                 </p>
