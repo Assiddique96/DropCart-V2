66367c9660a74925db44f7142f6ade73ce97daa3
Vincent <122548823+Vinnnnce@users.noreply.github.com>
2026-05-03 06:30:17 +0300

    Update page.jsx

--- app/(public)/page.jsx
+++ app/(public)/page.jsx
@@ -6,7 +6,7 @@ import OurSpecs from "@/components/OurSpec";
 import LatestProducts from "@/components/LatestProducts";
 
 export default function Home() {
-    return (.
+    return (
         <div>
             <Hero />
             <LatestProducts />
