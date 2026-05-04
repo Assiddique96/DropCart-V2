a47de570c6f207b79c0826efe4b5f1dea88cd522
Vincent <122548823+Vinnnnce@users.noreply.github.com>
2026-05-04 09:44:28 +0300

    Update ThemeProvider.jsx

--- components/ThemeProvider.jsx
+++ components/ThemeProvider.jsx
@@ -1,9 +1,15 @@
 "use client";
+
 import { ThemeProvider as NextThemesProvider } from "next-themes";
 
 export default function ThemeProvider({ children }) {
   return (
-    <NextThemesProvider attribute="class" defaultTheme="light" enableSystem={false}>
+    <NextThemesProvider
+      attribute="class"
+      defaultTheme="system"
+      enableSystem
+      disableTransitionOnChange
+    >
       {children}
     </NextThemesProvider>
   );
